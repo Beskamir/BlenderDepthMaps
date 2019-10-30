494547e1fc32784b992f467d01aee664dce2e76d
C M <christopher.mossman@ucalgary.ca>
2019-10-29 19:06:01 -0600

    Getting things set up for the images.

---
+++ images/Readme.txt
@@ -0,0 +1,6 @@
+0 = front
+1 = left
+2 = back
+3 = right
+4 = top
+5 = bottom
--- src/imageProcessing.py
+++ src/imageProcessing.py
@@ -1,8 +1,19 @@
+import bpy
 
+#References:
+#   https://docs.blender.org/api/blender_python_api_2_71_release/bpy.types.Image.html
+#   https://blender.stackexchange.com/questions/13422/crop-image-with-python-script/13516#13516
+#   https://blender.stackexchange.com/questions/3673/why-is-accessing-image-data-so-slow/3678#3678
+#   https://blender.stackexchange.com/questions/15890/is-it-possible-to-edit-images-programmatically-with-the-blender-api
+
+#bpy.data.images.load("path\to\file.jpg")
+
+IMAGE_PATH_PREFIX = "../fillthisin"
 def image():
     pass
 
 
 if __name__ == "__main__":
     # Test function
+    TEST_IMAGE_PATH_PREFIX = "../fillthisin"
     image()
