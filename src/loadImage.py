dcdd6e7d76addabfab491df6d0cc7c5ce7d69d9f
Sebastian Kopacz <sebastianvkopacz@gmail.com>
2019-11-13 18:40:58 -0700

    loads images using the efficient system that a stackexchange link in the file mentioned

--- src/loadImage.py
+++ src/loadImage.py
@@ -14,27 +14,43 @@ class loadImage:
     def __init__(self, imageFolder = "images"):
         self.imageDir = setupFilepath(imageFolder)
         self.openImages()
-        
+
+    def __del__(self):
+        # deconstructor that removes images added by the addon
+        for filename in os.listdir(self.imageDir):
+            # print("filename: "+filename)
+            for image in bpy.data.images:
+                # print("image: "+image.name)
+                if filename==image.name:
+                    bpy.data.images.remove(image)
+                #     print(image)
+            # print(self.imageDir+filename)
+            # image = bpy.data.images.load(self.imageDir+filename)
+        print("Cleaned up images")
+
     # Blender stores images as 1D arrays, ordered as described in https://blender.stackexchange.com/questions/13422/crop-image-with-python-script/13516#13516
     # This function generates a 2D array of (r,g,b,a) tuples from said 1D list
-    def blenderImageToTwoD(self, blenderImage):
+    def blenderImageTo2D(self, blenderImage):
+        print("loading image:"+blenderImage.name)
         image = []
+        pixels = blenderImage.pixels[:]
         x = blenderImage.size[0]
         y = blenderImage.size[1]
         index = 0
         for i in range(x):
             pixelRow = []
             for j in range(y):
-                r = blenderImage.pixels[index]
-                g = blenderImage.pixels[index+1]
-                b = blenderImage.pixels[index+2]
-                a = blenderImage.pixels[index+3]
+                r = pixels[index]
+                g = pixels[index+1]
+                b = pixels[index+2]
+                a = pixels[index+3]
                 pixelRow.append((r,g,b,a))
                 index += 4
             # As described in https://blender.stackexchange.com/questions/13422/crop-image-with-python-script/13516#13516
             # the topmost pixel rows come latest in the image.
             # Therefore, we insert rows at the "front" of our 2D array.
             image.insert(0, pixelRow)
+        # print(image)
         return image
 
     # Still need to write this one. Not as urgent.
@@ -46,9 +62,11 @@ class loadImage:
 
     def openImages(self):
         # This loops over every file in the identified images directory
+        loadedImages = []
         for filename in os.listdir(self.imageDir):
             # print(self.imageDir+filename)
             image = bpy.data.images.load(self.imageDir+filename)
+            loadedImages.append(self.blenderImageTo2D(image))
             # print("Loaded")
             # print(image)
 
@@ -72,8 +90,12 @@ if __name__ == "__main__":
     startTime = time.time()
     print("Running script...")
 
-    # Test the script itself
-    image = loadImage("Sphere")
+    # scope so that the image class gets deleted
+    def main():
+        # Test the script itself
+        image = loadImage("Sphere")
+        # del image
 
+    main()
     stopTime = time.time()
     print("Script took:",stopTime-startTime)
