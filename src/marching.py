86ef7a0cd7cd4e2f133b625de5d55216d4564552
Sebastian Kopacz <sebastianvkopacz@gmail.com>
2019-12-06 03:39:17 -0700

    used marching cube library which really increased performance, need to remove blender's dependency on using it's own python folder

--- src/marching.py
+++ src/marching.py
@@ -29,13 +29,73 @@ def genTriangles(triangles, name="marching cubes"):
     nextThreshold = 0
     pointsDone = 0
 
+    # print(triangles)
     # Setup the vertices for the object  
     for triangle in triangles:
+        # print(triangle)
         for v in triangle:
+            # print(v)
             bm.verts.new(v)
             pointsDone += 1
             if pointsDone > nextThreshold:
                 print(pointsDone, "vertices have been added so far.")
+                nextThreshold += printAfterCount
+    print("Calling to_mesh().")
+    bm.to_mesh(mesh)
+    print("Ensuring lookup table.")
+    bm.verts.ensure_lookup_table()
+
+    debugCounter = 0
+    debugSize = 50000
+    # Create the faces for the object
+    for i in range(0,len(bm.verts),3):
+        bm.faces.new( [bm.verts[i+0], bm.verts[i+1],bm.verts[i+2]])
+        debugCounter+=1
+        if(debugCounter%debugSize==0):
+            print("Created triangles",debugCounter,"/",len(bm.verts/3))
+
+    if bpy.context.mode == 'EDIT_MESH':
+        bmesh.update_edit_mesh(obj.data)
+    else:
+        bm.to_mesh(obj.data)
+    obj.data.update()
+    bm.free
+    print("Created shape")
+    # Merge overlapping vertices once everything's generated
+    # bpy.ops.object.editmode_toggle()
+    # bpy.ops.mesh.select_all(action='SELECT')
+    # bpy.ops.mesh.remove_doubles(threshold=0.0001)
+    # bpy.ops.object.editmode_toggle()
+    return obj
+
+# Based on voxelize.py code
+def genTrianglesFromVertices(vertices, faces, name="marching cubes"):
+    # For now, we'll combine the voxels from each of the six views into one array and then just take the unique values.
+    # Later on, this could be re-structured to, for example, render the voxels from each face in a separate colour
+
+    print("Number of triangles:", len(faces))
+    mesh = bpy.data.meshes.new("mesh")  # add a new mesh
+    obj = bpy.data.objects.new(name, mesh)
+    bpy.context.collection.objects.link(obj)  # put the object into the scene (link)
+    bpy.context.view_layer.objects.active = obj
+    obj.select_set(state=True)  # select object
+    mesh = obj.data
+    bm = bmesh.new()
+
+    print("Creating vertices...")
+    
+    printAfterCount = 100000
+    nextThreshold = 0
+    pointsDone = 0
+
+    # Setup the vertices for the object  
+    for triangle in faces:
+        for vertexIndex in triangle:
+            bm.verts.new(vertices[vertexIndex])
+            pointsDone += 1
+            if pointsDone > nextThreshold:
+                print("Vertices completed:",pointsDone,"/",len(vertices))
+                # print(pointsDone, "vertices have been added so far.")
                 nextThreshold += printAfterCount
     print("Calling to_mesh().")
     bm.to_mesh(mesh)
@@ -65,6 +125,15 @@ def genTriangles(triangles, name="marching cubes"):
     bpy.ops.object.editmode_toggle()
     return obj
 
+#TODO: This should be more effiecent but I'm not sure how to get external libraries to work in blender
+# uses https://scikit-image.org/docs/dev/api/skimage.measure.html#marching-cubes-lewiner for performance
+from skimage import measure
+def efficientMarchingCubes(image3D):
+    print("starting efficient marching cube alg")
+    verts, faces, normals, values = measure.marching_cubes_lewiner(image3D, level=0)
+    print("finished generating marching cubes")
+    # print("verts\n",verts,"\nfaces\n",faces,"\nnormals\n",normals,"\nvalues\n",values)
+    genTrianglesFromVertices(verts, faces)
 
 # Given a 3D array of -1's, 0's and 1's it'll place a marching cube based on the vertices that would be 0
 def imagesToMarchingInefficient(image3D):
@@ -520,6 +589,7 @@ if __name__ == "__main__":
     zMax = 32
     debugCounter=0
     debugAmount=100000
+    cellCap = (2*xMax)**3
     # for x in range(xMax):
     for x in range(-xMax,xMax):
         yArray = []
@@ -541,7 +611,7 @@ if __name__ == "__main__":
                 # zArray.append(randint(-1,1))
                 debugCounter+=1
                 if(debugCounter%debugAmount==0):
-                    print("genrated",debugCounter,"/",(2*xMax)**3)
+                    print("genrated",debugCounter,"/",cellCap)
             yArray.append(zArray)
             # yArray.append(randint(-1,1))
         testImageArray.append(yArray)
@@ -549,7 +619,8 @@ if __name__ == "__main__":
     # print(testImageArray)
     # print(len(testImageArray))
     # place voxels based on that 10*10*10 array
-    imagesToMarchingInefficient(testImageArray)
+    # imagesToMarchingInefficient(numpy.array(testImageArray))
+    efficientMarchingCubes(numpy.array(testImageArray))
     # imagesToVoxelsInefficient(testImageArray)
     # testImage = [[[0,0],[1,1]],[[1,1],[1,0]]]
     
